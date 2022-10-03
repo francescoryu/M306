5e30c02f12d2fae01034b445ac3ec72414ca2ddd
Francesco Ryu <francescoryu04@gmail.com>
2022-10-03 11:12:08 +0200

    added gui package

---
+++ src/main/java/GUI/DataGUI.java
@@ -0,0 +1,17 @@
+package GUI;
+
+import javax.swing.*;
+
+/**
+ * @author Francesco Ryu
+ * @version 1.0
+ * @since 2022-10-03
+ */
+
+public class DataGUI extends JFrame {
+    public DataGUI() {
+        setExtendedState(MAXIMIZED_BOTH);
+        setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class Main {
+import GUI.DataGUI;
+
+/**
+ * @author Francesco Ryu, Chris Hunziker,
+ * @version 1.0
+ * @since 2022-10-03
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        new DataGUI();
+    }
 }
