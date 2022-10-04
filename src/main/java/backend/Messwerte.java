844b428a0a7568f5c1f9954fc149fc49751ef868
Francesco Ryu <francescoryu04@gmail.com>
2022-10-04 20:29:36 +0200

    Changes by marko

--- src/main/java/backend/Messwerte.java
+++ src/main/java/backend/Messwerte.java
@@ -1,5 +1,10 @@
 package backend;
 
+import org.jfree.data.time.Day;
+import org.jfree.data.time.RegularTimePeriod;
+
+import java.sql.Timestamp;
+import java.util.Date;
 import java.util.Map;
 import java.util.TreeMap;
 
@@ -14,6 +19,9 @@ public class Messwerte {
     TreeMap<Long, Double> bezug = new TreeMap<Long, Double>();
     TreeMap<Long, Double> einspeisung = new TreeMap<Long, Double>();
 
+    TreeMap<Date, Double> bezugD = new TreeMap<Date, Double>();
+    TreeMap<Date, Double> einspeisungD = new TreeMap<Date, Double>();
+
     public Messwerte(){
     }
 
@@ -25,11 +33,53 @@ public class Messwerte {
         einspeisung.put(timestamp, messwert);
     }
 
+    /*
+    Test-Methode (nicht wichtig)
+     */
     public void printBezuege() {
         for (Map.Entry<Long, Double> entry : bezug.entrySet()) {
             System.out.println(entry.toString());
         }
     }
 
-    
+    /*
+    Diese Methode nimmt das Treemap mit den Bezuegen und wandelt die Keys von long zu Date.
+     */
+    public void toDateB() {
+        for (Map.Entry<Long, Double> entry : bezug.entrySet()) {
+            //RegularTimePeriod stamp = new Day(14,6,2000);
+            Date stamp = new Date(entry.getKey());
+            bezugD.put(stamp, entry.getValue());
+        }
+    }
+
+    /*
+    Diese Methode nimmet das Treemap mit der Einspeisung und wandelt die Keys von long zu Date.
+     */
+    public void toDateE() {
+        for (Map.Entry<Long, Double> entry : einspeisung.entrySet()) {
+            Date stamp = new Date(entry.getKey());
+            einspeisungD.put(stamp, entry.getValue());
+        }
+    }
+
+    /*
+    Getter-Methode der Bezuege-Treemap
+     */
+    public TreeMap<Long, Double> getMapB() {
+        return bezug;
+    }
+
+    /*
+    Getter-Methode der Einspeisung-Treemap
+     */
+    public TreeMap<Long, Double> getMapE() {
+        return einspeisung;
+    }
+
+    public void printBezuegeD() {
+        for (Map.Entry<Date, Double> entry : bezugD.entrySet()) {
+            System.out.println(entry.toString());
+        }
+    }
 }
